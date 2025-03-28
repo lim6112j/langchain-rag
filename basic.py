764c100165bd56fd909e5b2781815ee13dac967d
ben <Lim6112j@gmail.com>
2025-03-28 10:19:36 +0900

    bug resolved

--- basic.py
+++ basic.py
@@ -28,8 +28,8 @@ llm = init_chat_model("gpt-4o-mini", model_provider="openai")
 from langchain_openai import OpenAIEmbeddings
 embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
 from langchain_core.vectorstores import InMemoryVectorStore
-vector_store = InMemoryVectorStore(embeddings)
 
+vector_store = InMemoryVectorStore(embeddings)
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
 all_splits = text_splitter.split_documents(docs)
 _ = vector_store.add_documents(documents=all_splits)
@@ -39,19 +39,35 @@ prompt = hub.pull("rlm/rag-prompt")
 
 class State(TypedDict):
     question: str
-    convert: List[Document]
+    context: List[Document]
     answer: str
 
+# retrieve data from vector_store and handle over next tool `generate` with key named `context`
 def retrieve(state: State):
     retrieved_docs = vector_store.similarity_search(state["question"])
     return {"context": retrieved_docs}
 def generate(state: State):
-    docs_content = "\n\n".join(doc.page_content for doc in state["context"])
+    if "context" in state and state["context"]:
+        docs_content = "\n\n".join(doc.page_content for doc in state["context"])
+    else:
+        print("state['context'] not exists")
+        docs_content = ""
     messages = prompt.invoke({"question": state["question"], "context": docs_content})
     response = llm.invoke(messages)
     return {"answer": response.content}
 graph_builder = StateGraph(State).add_sequence([retrieve, generate])
 graph_builder.add_edge(START, "retrieve")
 graph = graph_builder.compile()
+
+from PIL import Image
+try:
+    d = graph.get_graph().draw_mermaid_png()
+    open('output.png', 'wb').write(d)
+    img = Image.open('output.png')
+    img.show()
+except Exception:
+    # This requires some extra dependencies and is optional
+    print(f"image exception:{Exception.with_traceback()}")
+
 response = graph.invoke({"question": "what is Task Decomposition?"})
 print(response["answer"])
