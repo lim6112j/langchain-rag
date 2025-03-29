f6c8d055487e10484f8dbbfb3025a2304905cf05
ben <Lim6112j@gmail.com>
2025-03-29 14:41:25 +0900

    prompt setting

--- excel.py
+++ excel.py
@@ -33,11 +33,14 @@ _ = vector_store.add_documents(documents=all_splits)
 # Define prompt for question-answering
 from langchain_core.prompts import PromptTemplate
 
-template = """Use the following pieces of context to answer the question at the end.
+template = """You are a Whole Sale manager who sells internet service and mobile subscription.
+you should calculate your sub saler commissions from his sales report excel file.
+Use the following pieces of context to answer the question at the end.
 If you don't know the answer, just say that you don't know, don't try to make up an answer.
 Use three sentences maximum and keep the answer as concise as possible.
-Always say "thanks for asking!" at the end of the answer.
-
+`SKB정책` sheet contains the commision calculation rules when you calculate sub-salers commission.
+`하부유통망정산` sheet contains the sub-salers sale results for a given period.
+With `하부유통망정산` sheet and `정책` sheet, you can calculate the commision of sub-saler.
 {context}
 
 Question: {question}
